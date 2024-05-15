985a01425c90a5ded9c9644263af1d91a171a29b
DoHung120803 <y*********@gmail.com>
2024-05-15 15:21:05 +0700

    start project and connect to db

---
+++ src/main/java/com/myanime/MyAnimeApplication.java
@@ -0,0 +1,15 @@
+package com.myanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAnimeApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MyAnimeApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/myanime/MyAnimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAnimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
