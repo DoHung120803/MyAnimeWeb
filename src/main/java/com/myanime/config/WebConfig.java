6ce8148fce8e87c81260c17a25a34d81c6db3f66
DoHung120803 <y*********@gmail.com>
2024-05-17 13:55:49 +0700

    Configure CORS by WebConfig class so that react (port 3000) receives data from spring boot (port 8080)

--- src/main/java/com/myanime/config/WebConfig.java
+++ src/main/java/com/myanime/config/WebConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/*
+*enable your Spring Boot application to handle CORS requests from your frontend application
+*running on http://localhost:3000
+*/
+
 @Configuration
 public class WebConfig {
 
