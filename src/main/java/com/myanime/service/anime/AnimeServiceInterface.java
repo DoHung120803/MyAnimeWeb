194a5376b1a5979b298e12e8a4602715fd010dbc
DoHung120803 <y*********@gmail.com>
2024-10-29 12:10:32 +0100

    save caches for get top animes

--- src/main/java/com/myanime/service/anime/AnimeServiceInterface.java
+++ src/main/java/com/myanime/service/anime/AnimeServiceInterface.java
@@ -1,7 +1,6 @@
 package com.myanime.service.anime;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.myanime.entity.jpa.Anime;
 import com.myanime.model.dto.request.anime.AnimeCreationRequest;
 import com.myanime.model.dto.request.anime.AnimeUpdateRequest;
 import com.myanime.model.dto.response.AnimeResponse;
@@ -17,7 +16,5 @@ public interface AnimeServiceInterface {
     AnimeResponse updateAnime(String id, AnimeUpdateRequest request);
     void deleteAnime(String id);
     List<AnimeResponse> findAnimeByName(String name);
-    List<Anime> getTopViewsAnimes();
-    List<Anime> getTopRateAnimes();
-
+    List<AnimeResponse> getTopAnimes(String type) throws JsonProcessingException;
 }
